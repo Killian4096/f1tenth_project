714e7aef0146bd4e95a03716954107240ef9469c
Killian4096 <killianrush203@gmail.com>
2022-02-12 11:53:53 -0500

    Finally got crash detection somewhat working

--- src/wall_avoidance/nodes/crash_detection.py
+++ src/wall_avoidance/nodes/crash_detection.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from time import time
+from turtle import distance
 from xml.etree.ElementTree import TreeBuilder
 import rospy
 import std_msgs.msg
@@ -9,7 +10,9 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 import math
 
-TIME_THRESHOLD = 0.3
+import numpy as np
+
+TIME_THRESHOLD = 0.6
 DIST_THRESHOLD = 5
 CAR_SIZE = 0.08
 
@@ -20,14 +23,12 @@ rate = None
 class crash_detection:
     def __init__(self):
 
-        self.odom_data_current = Odometry()
-        self.odom_data_past = Odometry()
+        self.odom_data = Odometry()
 
         self.brake_bool_pub = rospy.Publisher('/brake_bool', std_msgs.msg.Bool, queue_size=10)
         self.brake_pub = rospy.Publisher('/brake', ackermann_msgs.msg.AckermannDriveStamped, queue_size=10)
         self.LaserScan_sub = rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)
         self.Odometry_sub = rospy.Subscriber('/odom', Odometry, self.odometry_callback)
-        self.Debug_Vector_pub = rospy.Publisher('/debug_vector', LaserScan, queue_size=10)
     
     def laserscan_callback(self, laserscan_data):
         if(self.detect_potential_collision(laserscan_data, TIME_THRESHOLD)):
@@ -37,62 +38,45 @@ class crash_detection:
 
     def detect_potential_collision(self, laserscan_data, time_threshold):
         time_impact_field = self.get_time_impact_field(laserscan_data)
-        for i in range(0, len(time_impact_field)):
-            if (time_impact_field[i] <= rospy.rostime.Duration(time_threshold,0).to_nsec()) and (time_impact_field[i] != 0):
-                return True
+        if min(time_impact_field) <= rospy.rostime.Duration(time_threshold,0).to_sec():
+            return True
         return False
 
     def get_time_impact_field(self, laserscan_data):
         #Return time till impact s
-        time_impact_field = []
         speed_field = self.get_speed_field(laserscan_data)
         distance_field = self.get_distance_field(laserscan_data)
-        for i in range(0, len(distance_field)):
-            time_impact_field.append(distance_field[i] / speed_field[i])
-            if(time_impact_field[i]<0):
-                time_impact_field[i]=0
-        debug_scan = self.create_laserscan_message(laserscan_data, time_impact_field)
-        self.Debug_Vector_pub.publish(debug_scan)
+        time_impact_field = np.divide(distance_field, speed_field)
         return time_impact_field
 
     def get_speed_field(self, laserscan_data):
         #Returns speed field m/s
-        speed = self.get_speed(self.odom_data_current, self.odom_data_past)
-        speed_field = []
-        for i in range(0, len(laserscan_data.ranges)):
-            speed_field.append(speed * math.cos(laserscan_data.angle_min + (i * laserscan_data.angle_increment)))
+        speed = self.get_speed(self.odom_data)
+        angle_field = np.arange(laserscan_data.angle_min, laserscan_data.angle_max, laserscan_data.angle_increment)
+        speed_field = speed * np.cos(angle_field)
+        speed_field[speed_field <= 0] = 0.00001
         return speed_field
     
     def get_distance_field(self, laserscan_data):
         #Return ranges m
-        distance_field = []
-        for i in range(0, len(laserscan_data.ranges)):
-            distance_field.append(laserscan_data.ranges[i] - CAR_SIZE)
+        distance_field = np.array(laserscan_data.ranges)
         return distance_field
 
-    def get_speed(self, odom_data_current, odom_data_past):
+    def get_speed(self, odom_data):
         #Return speeds m/s
-        dx = (odom_data_current.pose.pose.position.x - odom_data_past.pose.pose.position.x)
-        dt = (odom_data_current.header.stamp - odom_data_past.header.stamp)
-        speed=(dx+0.001)/(dt.to_nsec() + rospy.rostime.Duration(0,1).to_nsec())
+        speed = odom_data.twist.twist.linear.x
         return speed
 
 
     def odometry_callback(self, odom_data):
         #Dont want the 2 having the same value, hence the buffer
-        odom_buffer = self.odom_data_current
-        self.odom_data_current = odom_data
-        self.odom_data_past = odom_buffer
-
+        self.odom_data = odom_data
         self.brake_pub.publish(self.create_brake_ackermann_message(odom_data))
 
     def create_brake_ackermann_message(self, message_origin):
-        return ackermann_msgs.msg.AckermannDriveStamped(message_origin.header, ackermann_msgs.msg.AckermannDrive(0,0,0,0,0))
-    
-    def create_laserscan_message(self, laserscan_data, ranges):
-        return LaserScan(laserscan_data.header, laserscan_data.angle_min, laserscan_data.angle_max, laserscan_data.angle_increment, laserscan_data.time_increment, laserscan_data.scan_time, laserscan_data.range_min, laserscan_data.range_max, ranges, laserscan_data.intensities)
-
-
+        message = ackermann_msgs.msg.AckermannDriveStamped()
+        message.drive.speed = 0.0
+        return message
 
 def main():
     rospy.init_node('crash_detection')
