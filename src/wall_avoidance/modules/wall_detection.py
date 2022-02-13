a11acafefe114d4b10695d65420d62668629cbf7
Killian4096 <killianrush203@gmail.com>
2022-02-13 00:43:05 -0500

    Did some more work in modules

--- src/wall_avoidance/modules/wall_detection.py
+++ src/wall_avoidance/modules/wall_detection.py
@@ -9,6 +9,7 @@ import math
 import numpy as np
 
 ERROR_THRESHOLD = (3.14)/16
+CONSECUTIVE_POINTS = 10
 
 class wall_detection:
     def __init__(self):
@@ -16,9 +17,42 @@ class wall_detection:
 
     def wall_detect(self, laserscan_data):
         errors = self.get_errors(laserscan_data)
-        if(np.greater(errors, ERROR_THRESHOLD)):
-            return True
-        return False
+        wall_points = self.create_wall_points(laserscan_data)
+
+    def create_wall_points(self, laserscan_data):
+        wall_points = np.array()
+        points = self.create_points(laserscan_data)
+        i=0
+        while i < len(points) - 1:
+            wall_points.append(range_wall(points[i], points[i+1]))
+
+    def create_points(self, laserscan_data):
+        errors = self.get_errors(laserscan_data)
+        positions = self.get_positions(laserscan_data)
+        positions_x = positions[0]
+        positions_y = positions[1]
+        points = np.array()
+        i = 0
+        line = True
+        consec = 0
+        while (i < len(errors) and len(points)%2==0):
+            if line:
+                if errors[i] >= ERROR_THRESHOLD:
+                    line = False
+                    points.append(positions_x[i], positions_y[i])
+            else:
+                if errors[i] <= ERROR_THRESHOLD:
+                    consec += 1
+                    if consec == CONSECUTIVE_POINTS:
+                        consec = 0
+                        line = True
+                        points.append([positions_x[i-CONSECUTIVE_POINTS], positions_y[i - CONSECUTIVE_POINTS]])
+                else:
+                    consec = 0
+        if(points[1] == points[-1]) and (len(points)>2):
+            points = np.delete(points, 0)
+            points = np.delete(points, 0)
+
     
     def get_errors(self, laserscan_data):
         positions = self.get_positions()
@@ -31,8 +65,6 @@ class wall_detection:
                 np.add(np.roll(positions_x, -2), np.subtract(np.roll(positions_y,  0), np.roll(positions_y  -1)))
         )
         return errors
-
-    
 
     def get_ranges(self, laserscan_data):
         ranges = np.array(laserscan_data.ranges)
@@ -52,6 +84,6 @@ class wall_detection:
         return positions
 
 class range_wall:
-    def __init__():
-        self.point1 = None
-        self.point2 = None
+    def __init__(point1, point2):
+        self.point1 = point1
+        self.point2 = point2
